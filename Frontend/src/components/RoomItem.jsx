77a51c1f5029f54582f56905fcc3fdd2534ec3ff
anuragk-04 <anurag04kushwaha@gmail.com>
2025-06-16 00:25:02 +0530

    bug fixed in roomCard

--- Frontend/src/components/RoomItem.jsx
+++ Frontend/src/components/RoomItem.jsx
@@ -69,14 +69,29 @@ const RoomItem = ({ roomId, name, description, role, memberCnt, lastAccessedAt }
             <Card>
                 <CardContent>
                     <Grid container>
-                        <Grid item xs={6}>
-                            <Typography variant="h6" component="div">
-                                {name}
-                            </Typography>
-                            <Typography variant="body2">{description}</Typography>
+                        <Grid container direction={'column'} >
+                            <Grid item xs={6}>
+                                <Typography variant="h6" component="div">
+                                    {name}
+                                </Typography>
+                                <Typography variant="body2">{description}</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography variant="body2" className='mt-5'>
+                                    <Typography variant="body3"> 
+                                        <span className='f-bold'>Role: </span> {role} 
+                                    </Typography>
+                                    <Typography variant="body3"> 
+                                        <span className='f-bold ml-20'>Members: </span> {memberCnt} 
+                                    </Typography>
+                                    <Typography variant="body3"> 
+                                        <span className='f-bold ml-20'>Last accessed at:</span> { lastAccessedAt === null ? 'you haven\'t accessed yet.' : new Date().toDateString()} 
+                                    </Typography>
+                                </Typography>
+                            </Grid>
                         </Grid>
                         <Grid item xs={6}>
-                            <Stack direction="row" spacing={1}>
+                            <Stack direction="row" spacing={2}>
                                 {(role === 'EDITOR' || role === 'OWNER') && 
                                     <Button variant="outlined" color='error' startIcon={<DeleteIcon />} onClick={handleDeleteRoom}>
                                         Delete
@@ -90,19 +105,7 @@ const RoomItem = ({ roomId, name, description, role, memberCnt, lastAccessedAt }
                                     </Button>}
                             </Stack>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant="body2" className='mt-5'>
-                                <Typography variant="body3"> 
-                                    <span className='f-bold'>Role: </span> {role} 
-                                </Typography>
-                                <Typography variant="body3"> 
-                                    <span className='f-bold ml-20'>Members: </span> {memberCnt} 
-                                </Typography>
-                                <Typography variant="body3"> 
-                                    <span className='f-bold ml-20'>Last accessed at:</span> { lastAccessedAt === null ? 'you haven\'t accessed yet.' : new Date().toDateString()} 
-                                </Typography>
-                            </Typography>
-                        </Grid>
+                        
                     </Grid>
                 </CardContent>
             </Card>
